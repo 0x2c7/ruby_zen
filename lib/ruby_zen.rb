0efaf4484860c07ebe36f6bc83d79d33a6340e5b
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-29 01:03:44 +0700

    Move class indexer out of engine

--- lib/ruby_zen.rb
+++ lib/ruby_zen.rb
@@ -1,4 +1,5 @@
 require 'ruby_zen/version'
+require 'ruby_zen/indexers/class_indexer'
 require 'ruby_zen/class_object'
 require 'ruby_zen/method_object'
 require 'ruby_zen/engine'
--- lib/ruby_zen/engine.rb
+++ lib/ruby_zen/engine.rb
@@ -1,19 +1,18 @@
 module RubyZen
   class Engine
-    attr_reader :classes
-
     INTERNAL_CLASSES = %w[
       Kernel Method Object BasicObject Encoding Comparable Enumerable String
       Exception Numeric Bignum Array Hash Struct Regexp Range IO Dir Time Random
       Proc Binding Math GC Enumerator Thread
     ].freeze
-    SINGLETON_CLASS_PATTERN = /^\#\<Class\:([^\>]+)\>$/
 
     def initialize(logger:)
       @classes = {}
       @logger = logger
 
-      init_internal_classes
+      @logger.info('Starting RubyZen engine...')
+      index_internal_classes
+      @logger.info('RubyZen is ready!')
     end
 
     def index_iseq(iseq)
@@ -21,71 +20,35 @@ module RubyZen
     end
 
     def index_class(klass)
-      class_object = find_or_create_class_object(klass)
+      RubyZen::Indexers::ClassIndexer.new(self, klass, logger: @logger).start
+    end
 
-      klass.instance_methods.each do |method_name|
-        method_object = create_method_object(klass.instance_method(method_name))
-        class_object.add_method(method_object)
+    def fetch_class(class_name)
+      @classes[class_name]
+    end
+
+    def define_class(class_name)
+      if @classes[class_name]
+        @classes[class_name]
+      elsif block_given?
+        @classes[class_name] = yield
+      else
+        raise 'This method requires a block'
       end
-
-      klass.methods.each do |method_name|
-        method_object = create_method_object(klass.method(method_name))
-        class_object.add_class_method(method_object)
-      end
-
-      @logger.debug("Finish indexing #{klass}")
     end
 
     private
 
-    def init_internal_classes
+    def index_internal_classes
       INTERNAL_CLASSES.each do |class_name|
         begin
           klass = Object.const_get(class_name)
-          index_class(klass)
+          RubyZen::Indexers::ClassIndexer.new(
+            self, klass, logger: @logger).start
         rescue NameError => e
           @logger.debug("Fail to index #{class_name}: #{e}")
         end
       end
-    end
-
-    def find_or_create_class_object(klass)
-      return nil if klass.nil?
-
-      if klass.name.nil?
-        # Singleton class
-        match = SINGLETON_CLASS_PATTERN.match(klass.to_s)
-        return nil unless match
-        original_klass = Object.const_get(match[1])
-        @classes[original_klass.name] ||= RubyZen::ClassObject.new(
-          original_klass.name,
-          superclass: find_or_create_class_object(original_klass.superclass)
-        )
-        @classes[original_klass.name].singleton_class
-      elsif !klass.respond_to?(:superclass)
-        # Module
-        @classes[klass.name] ||= RubyZen::ClassObject.new(
-          klass.name,
-          is_module: true
-        )
-      else
-        # Normal class
-        @classes[klass.name] ||= RubyZen::ClassObject.new(
-          klass.name,
-          superclass: find_or_create_class_object(klass.superclass)
-        )
-      end
-    end
-
-    def create_method_object(native_method)
-      return nil if native_method.nil?
-
-      RubyZen::MethodObject.new(
-        native_method.name,
-        owner: find_or_create_class_object(native_method.owner),
-        super_method: create_method_object(native_method.super_method),
-        parameters: native_method.parameters
-      )
     end
   end
 end
