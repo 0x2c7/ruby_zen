e9b1b5fc5bd0b8733850bc36566e833005fb3b7f
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-29 02:39:35 +0700

    Add example file

---
+++ test.rb
@@ -0,0 +1,14 @@
+require 'yarv_generator'
+require 'pp'
+require 'ruby_zen'
+require 'logger'
+require 'byebug'
+
+code = File.read('./spec/definitions/method_1.rb')
+iseq = YarvGenerator.build_from_source(code)
+puts iseq.inspect
+
+logger = Logger.new(STDOUT)
+engine = RubyZen::Engine.new(logger: logger)
+engine.index_iseq(iseq)
+pp engine.fetch_class('Order').instance_method_objects(false)
