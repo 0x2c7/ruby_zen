ffb0785aa86790d50a974cf18a304c0b3e2edc50
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-29 23:58:25 +0700

    Implement RubyZen VM

--- lib/ruby_zen.rb
+++ lib/ruby_zen.rb
@@ -4,6 +4,7 @@ require 'ruby_zen/indexers/iseq_indexer'
 require 'ruby_zen/double_stack'
 require 'ruby_zen/class_object'
 require 'ruby_zen/method_object'
+require 'ruby_zen/vm'
 require 'ruby_zen/engine'
 
 module RubyZen
