f48cb076c571f01b79c37ee600c5053236be44b6
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-29 23:57:59 +0700

    Implement DoubleStack

--- lib/ruby_zen.rb
+++ lib/ruby_zen.rb
@@ -1,6 +1,7 @@
 require 'ruby_zen/version'
 require 'ruby_zen/indexers/class_indexer'
 require 'ruby_zen/indexers/iseq_indexer'
+require 'ruby_zen/double_stack'
 require 'ruby_zen/class_object'
 require 'ruby_zen/method_object'
 require 'ruby_zen/engine'
