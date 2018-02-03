501000930072baa2d1aac19a0d65d69abf8949ab
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-02-03 10:06:41 +0700

    Change test file

--- test.rb
+++ test.rb
@@ -3,11 +3,14 @@ require 'pp'
 require 'ruby_zen'
 require 'logger'
 
-code = File.read('./spec/definitions/method_1.rb')
+code = File.read('./spec/definitions/class_method_1.rb')
 iseq = YarvGenerator.build_from_source(code)
-puts iseq.inspect
 
 logger = Logger.new(STDOUT)
+logger.level = Logger::INFO
 engine = RubyZen::Engine.new(logger: logger)
 engine.index_iseq(iseq)
-pp engine.fetch_class('Order').instance_method_objects(false)
+puts "========== Instance Methods =========="
+pp engine.fetch_class('Contract').instance_method_objects(false)
+puts "========== Class Methods =========="
+pp engine.fetch_class('Contract').class_method_objects(false)
