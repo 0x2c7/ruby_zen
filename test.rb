ddc090062a293d9a5c152a05a4c58a87a4c563f2
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-02-03 10:38:51 +0700

    Refactor test files

--- test.rb
+++ test.rb
@@ -7,24 +7,19 @@ logger = Logger.new(STDOUT)
 logger.level = Logger::INFO
 engine = RubyZen::Engine.new(logger: logger)
 
-code = File.read('./spec/definitions/method_1.rb')
-iseq = YarvGenerator.build_from_source(code)
-puts "========== Sample Code =========="
-puts code
-puts "========== Indexing code =========="
-engine.index_iseq(iseq)
-puts "========== Instance Methods =========="
-pp engine.fetch_class('Order').instance_method_objects(false)
-puts "========== Class Methods =========="
-pp engine.fetch_class('Order').class_method_objects(false)
+def test_index(file, klass, engine)
+  code = File.read(file)
+  iseq = YarvGenerator.build_from_source(code)
+  puts "========== Sample Code =========="
+  puts code
+  puts "========== Indexing code =========="
+  engine.index_iseq(iseq)
+  puts "========== Instance Methods =========="
+  pp engine.fetch_class(klass).instance_method_objects(false)
+  puts "========== Class Methods =========="
+  pp engine.fetch_class(klass).class_method_objects(false)
+end
 
-code = File.read('./spec/definitions/class_method_1.rb')
-iseq = YarvGenerator.build_from_source(code)
-puts "========== Sample Code =========="
-puts code
-puts "========== Indexing code =========="
-engine.index_iseq(iseq)
-puts "========== Instance Methods =========="
-pp engine.fetch_class('Contract').instance_method_objects(false)
-puts "========== Class Methods =========="
-pp engine.fetch_class('Contract').class_method_objects(false)
+test_index('./spec/definitions/method_1.rb', 'Order', engine)
+test_index('./spec/definitions/class_method_1.rb', 'Contract', engine)
+
