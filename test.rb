d6d7a7886980f839f9a7f5a29667439c4608b473
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-02-03 10:12:24 +0700

    Add more info into test file

--- test.rb
+++ test.rb
@@ -3,12 +3,26 @@ require 'pp'
 require 'ruby_zen'
 require 'logger'
 
-code = File.read('./spec/definitions/class_method_1.rb')
-iseq = YarvGenerator.build_from_source(code)
-
 logger = Logger.new(STDOUT)
 logger.level = Logger::INFO
 engine = RubyZen::Engine.new(logger: logger)
+
+code = File.read('./spec/definitions/method_1.rb')
+iseq = YarvGenerator.build_from_source(code)
+puts "========== Sample Code =========="
+puts code
+puts "========== Indexing code =========="
+engine.index_iseq(iseq)
+puts "========== Instance Methods =========="
+pp engine.fetch_class('Order').instance_method_objects(false)
+puts "========== Class Methods =========="
+pp engine.fetch_class('Order').class_method_objects(false)
+
+code = File.read('./spec/definitions/class_method_1.rb')
+iseq = YarvGenerator.build_from_source(code)
+puts "========== Sample Code =========="
+puts code
+puts "========== Indexing code =========="
 engine.index_iseq(iseq)
 puts "========== Instance Methods =========="
 pp engine.fetch_class('Contract').instance_method_objects(false)
