a7cf670573d521235b92492a1785cac01795d360
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-03-18 11:58:50 +0700

    Remove useless tests

--- spec/ruby_zen_spec.rb
+++ spec/ruby_zen_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe RubyZen do
   it "has a version number" do
     expect(RubyZen::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
