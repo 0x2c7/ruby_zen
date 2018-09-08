39c623eb4378fd635aa85616ae6bddbf52221f4d
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-09-08 12:23:55 +0700

    Add ReturnObject and move to sub-folder

---
+++ lib/ruby_zen/objects/return_object.rb
@@ -0,0 +1,28 @@
+require 'set'
+
+module RubyZen
+  class ReturnObject
+    attr_reader :parent
+
+    def initialize(parent)
+      @parent        = parent
+      @possibilities = Set.new
+    end
+
+    def add(value)
+      @possibilities.add(value)
+    end
+
+    def to_set
+      possibilities = Set.new
+      @possibilities.each do |possibility|
+        if possibility.is_a?(self.class)
+          possibilities += possibility.to_set
+        else
+          possibilities << possibility
+        end
+      end
+      possibilities
+    end
+  end
+end
